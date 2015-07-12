3d913fade3a7d493f8ced54160647fcdbf5a0c2a
geekgeek <solq87@gmail.com>
2015-07-12 21:12:34 +0200

    Create list_loop_all_values.py

---
+++ list_loop_all_values.py
@@ -0,0 +1,11 @@
+#Using list and print out everything in list
+
+print('\n')
+grocery_list = ['juice','tomatoes','potatoes','bananas',22]
+
+for y in grocery_list:
+    print(y)
+
+
+#Add values to list
+grocery_list.append('chips')
