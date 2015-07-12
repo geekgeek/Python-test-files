eec731073ee64d0a5790525a495f82f819d37a76
geekgeek <solq87@gmail.com>
2015-07-12 22:05:48 +0200

    Create string_replace_words2.py

---
+++ string_replace_words2.py
@@ -0,0 +1,8 @@
+#http://stackoverflow.com/questions/5082452/python-string-formatting-vs-format
+
+#Str
+li = [12,45,78]
+print map('the number is {}'.format,li)
+
+#Prints out:
+# ['the number is 12', 'the number is 45', 'the number is 78']
